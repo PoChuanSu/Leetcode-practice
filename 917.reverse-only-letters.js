5c43b3644248ee975af33090f1162af727c7fd2a
PoChuanSu <peterpeter1010@live.com>
2024-11-23 17:52:22 +1100

    reverse only letter

--- 917.reverse-only-letters.js
+++ 917.reverse-only-letters.js
@@ -10,7 +10,34 @@
  * @return {string}
  */
 var reverseOnlyLetters = function(s) {
-    
+    s = s.split('');
+    let i = 0;
+    let j = s.length -1;
+
+    while (i <= j) {
+        if (s[i].match(/[a-zA-Z]/) && !s[j].match(/[a-zA-Z]/)){
+            j--;
+        }
+
+        else if (!s[i].match(/[a-zA-Z]/) && s[j].match(/[a-zA-Z]/)){
+            i++;
+        }
+
+        else if (!s[i].match(/[a-zA-Z]/) && !s[j].match(/[a-zA-Z]/)){
+            i++;
+            j--;
+        }
+
+        else {
+            let tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
+            i++;
+            j--;
+        }
+    }
+
+    return s.join('');
 };
 // @lc code=end
 
