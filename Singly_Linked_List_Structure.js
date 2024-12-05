433da7cc0e8bb43ff7d0869ab57c4e0092086d27
PoChuanSu <peterpeter1010@live.com>
2024-12-05 17:16:13 +1100

    pop

--- Singly_Linked_List_Structure.js
+++ Singly_Linked_List_Structure.js
@@ -27,4 +27,31 @@ class SinglyLinkedList{
         this.length ++;
         return this;
     }
+
+    traverse () {
+        var current = this.head;
+        while(current) {
+            current = current.next;
+        }
+    }
+
+    pop () {
+        if (!this.head) return undefined;
+
+        var current = this.head;
+        var newTail = current;
+        while(current.next) {
+            newTail = current;
+            current = current.next;
+        }
+
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length --;
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
 }
