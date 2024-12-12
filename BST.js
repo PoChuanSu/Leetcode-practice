b5907349b67cdc8fba8343ccb7d63bfb0f032504
PoChuanSu <peterpeter1010@live.com>
2024-12-12 15:33:35 +1100

    tree BFS

--- BST.js
+++ BST.js
@@ -59,4 +59,20 @@ class BinarySearchTree {
         if (current) return current;
         else return undefined;
     }
+
+    BFS() {
+        var prio = [],
+            data = [],
+            node = this.root;
+
+        queue.push(node);
+
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        return data;
+    }
 }
